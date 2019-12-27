4f0966442a33f8dd3704e6443a9ec76f3c978ae2
duanmu <duanmujm@gobestsoft.com>
2019-12-27 13:47:50 +0800

    Eureka服务注册

---
+++ shop-eureka-server/src/main/java/com/khunboo/eureka/ShopEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.khunboo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ShopEurekaServerApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ShopEurekaServerApplication.class, args);
+    }
+}
